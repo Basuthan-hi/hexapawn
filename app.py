a80eff0c41d2d8474ef70f330fc31b39cc6ce9c8
Basuthan-hi <157631478+Basuthan-hi@users.noreply.github.com>
2024-03-09 22:39:30 +0530

    finising placement and destroy

--- app.py
+++ app.py
@@ -1,34 +1,30 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 app = Ursina()
-player = FirstPersonController()
-for z in range(20):
-    for x in range(20):
-        Entity(
-           parent=scene,
-            position=(x,0,z),
-            origin_y =0.5,
-            color = color.rgb(255,255,255),
-            texture= 'texture/grass.jpg',
-            model = 'cube',
-            collider = 'box',
-            ignore = True,
-            text = 'white cube'
-        )
-class TextureBox(Button):
+block = 'object/block'
+grass_texture = load_texture('assets/grass_block.png')
+class Block(Button):
     def __init__(self, position=(5,2,5)):
         super().__init__(
-            parent=scene,
-            position=position,
-            origin_y =0.5,
-            color = color.rbg(0,0,1),
-            texture= 'texture/grass.jpg',
-            model = 'cube'
+            parent = scene,
+			position = position,
+			model = block,
+			origin_y = 0.5,
+			texture = grass_texture,
+			color = color.color(0,0,random.uniform(0.9,1)),
+			scale = 0.5
         )
-        self.texture_index = 0
-        self.texture = ['grass.jpg']
-    def input(self, key):
-        return None
-TextureBox()
+    def input(self,key):
+        if self.hovered:
+            if key == "left mouse down":
+                block = Block(position=self.position + mouse.normal)
+            elif key == "right mouse down":
+                destroy(self)
+
+for z in range(20):
+    for x in range(20):
+        block = Block(position=(x,0,z))
+
+player = FirstPersonController()
 if __name__ == '__main__':
     app.run()
